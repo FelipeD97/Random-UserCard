d2384067b3e9f9a891d2a9bc29cf6841a30a4f24
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2019-11-13 11:06:09 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Random User Cards
+
+This project was bootstrapped with [Create React App].
+
+## Description
+
+This is a simple React app that will randomly create user profile cards. This also includes test cases.
+
+## Technologies Used:
+
+1. React
+2. CSS
+
+## API used:
+
+1. https://randomuser.me/api/?results=2
---
+++ src/components/cardList.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Styled from "styled-components";
+import { loadData } from "../utils/loadData";
+import Card from "../components/card";
+
+const List = Styled.ul`
+    list-style-type: none;
+    margin: 1rem auto;
+    padding: 16px;
+`;
+
+class CardList extends Component {
+    state = {
+        users: []
+    };
+
+    async componentDidMount() {
+        const response = await loadData(`https://randomuser.me/api/?results=2`);
+
+        { /**Specifies what information we want from API through devtools **/ }
+        const users = response.results;
+        this.setState({
+            users
+        });
+    }
+
+
+  render() {
+      const { users } = this.state;
+    return (
+      <List>
+        {users.map(user => (
+            <li key={user.login.uuid}>
+            <Card user={user} />
+            </li>
+        ))}
+      </List>
+    );
+  }
+}
+
+export default CardList;
